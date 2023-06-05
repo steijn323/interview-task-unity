12eee43b0a84e950b267935fd500a7d28964c3b6
nicolassteijn <nicolas.steijn.it@gmail.com>
2023-06-05 21:54:40 +0200

    + readme

--- dockerfile
+++
@@ -1,3 +0,0 @@
-FROM eclipse-temurin:17-jdk-alpine
-COPY build/libs/repository-task-0.0.1-SNAPSHOT.jar repository-task-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java","-jar","/repository-task-0.0.1-SNAPSHOT.jar"]
--- src/main/java/pl/unityt/repositorytask/service/GithubRepoService.java
+++
@@ -1,8 +0,0 @@
-package pl.unityt.repositorytask.service;
-
-import pl.unityt.repositorytask.dto.RepositoryDetailsDto;
-
-public interface GithubRepoService {
-
-    RepositoryDetailsDto fetchRepoDetails(String owner, String repositoryName);
-}
